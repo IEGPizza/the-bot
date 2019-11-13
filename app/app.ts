02e8ba9e277f3fb2927e1acb984f979bd6ffe87f
IEGPizza <wandschrank5@gmx.de>
2019-11-13 02:20:43 +0100

    first prototype that works, updated package.json

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import { ContextMessageUpdate } from 'telegraf';
 
 const dotenv = require('dotenv');
 const Telegraf = require('telegraf');
+const request = require('request');
 
 
 dotenv.config();
@@ -15,11 +16,20 @@ bot.catch((err: any, ctx: ContextMessageUpdate) => {
 });
 
 bot.start((ctx: ContextMessageUpdate) => {
+    console.log(`received message from user ${ctx.from?.id}`);
     if (ctx.from?.id == process.env.MY_TELEGRAM_USER_ID) {
         ctx.reply('ok');
-        console.log(ctx.chat?.id);
-        bot.telegram.sendDocument(ctx.chat?.id, 'http://algo.cs.uni-frankfurt.de/lehre/algo2/winter1920/uebung/gl11920_uebung03.pdf');
+        console.log(`chat id is ${ctx.chat?.id}`);
+        checkForPDFs();
+    } else {
+        ctx.reply('hi :D');
+        // bot.telegram.sendDocument(ctx.chat?.id, 'http://algo.cs.uni-frankfurt.de/lehre/algo2/winter1920/uebung/gl11920_uebung03.pdf');
     }
+});
+
+bot.on('message', (ctx: ContextMessageUpdate) => {
+    console.log(`sending sticker to user ${ctx.from?.id}`);
+    return bot.telegram.sendSticker(ctx.chat?.id, 'CAADAgADQAEAAhZ8aAPOt9pjb9XRXRYE');
 });
 
 bot.launch();
@@ -29,3 +39,13 @@ console.log('bot running');
 
 
 
+function checkForPDFs() {
+
+    // request.get(`http://algo.cs.uni-frankfurt.de/lehre/algo2/winter1920/uebung/gl11920_uebung${getTwoDigitNumber()}.pdf`, (error: any, response: Response, body: any) => {
+    //     console.log(JSON.stringify(error), response.statusCode);
+    // });
+}
+
+function getTwoDigitNumber(a: number): string {
+    return a < 10 ? '0' + a.toString() : a.toString();
+}
